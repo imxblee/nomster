0d83f30de59e96dda3854504a0ad4bde74e1b8d6
imxblee <transcendencez@gmail.com>
2015-12-24 14:10:21 +0000

    Stuck at simpleform @photo

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Nomster::Application.routes.draw do
   root 'places#index'
   resources :places do 
     resource :comments,:only => :create
+    resource :photos,:only => :create
   end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
